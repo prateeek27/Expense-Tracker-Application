1c293995a557efd5df86df4ad31c7b0381def0c6
Prateek Bansal <prateekbansal416@hotmail.com>
2024-12-08 22:00:56 +0530

    Revert "Added Deployment config"
    This reverts commit b6cff0f731eebded5c6376536095ff2292220df1.

--- src/main/java/com/project/expensetrackerapp/ExpenseTrackerApplication.java
+++ src/main/java/com/project/expensetrackerapp/ExpenseTrackerApplication.java
@@ -3,8 +3,6 @@ package com.project.expensetrackerapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.Collections;
-
 /**
  * Main class for the Expense Tracker application.
  * This is the entry point of the Spring Boot application.
@@ -12,16 +10,12 @@ import java.util.Collections;
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
-    /**
-     * Main method which starts the Spring Boot application.
-     *
-     * @param args command-line arguments
-     */
-    public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(ExpenseTrackerApplication.class);
-        if (System.getenv("PORT") != null) {
-            app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
-        }
-        app.run(args);
-    }
+	/**
+	 * Main method which starts the Spring Boot application.
+	 *
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
 }
